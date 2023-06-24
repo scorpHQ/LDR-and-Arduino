77e7482db30fbf2b4721d37fd7a4a0b35d48bf06
I Made Hendry Keswara <131842648+hendrykeswara@users.noreply.github.com>
2023-06-24 16:31:49 +0700

    Add remark code and delay 1 second

--- Code.ino
+++ Code.ino
@@ -1,31 +1,31 @@
-const int ledPin = 13;
-const int ldrPin = A0;
+const int ledPin = 13; //LED Declaration on Arduino pin 13
+const int ldrPin = A0; //LDR Declaration on Arduino pin A0
 
 
 void setup() {
 
 Serial.begin(9600);
-pinMode(ledPin, OUTPUT);
-pinMode(ldrPin, INPUT);
+pinMode(ledPin, OUTPUT); //LED Declaration as output
+pinMode(ldrPin, INPUT); //LDR Declaration as input
 
 }
 
 
 void loop() {
 
-int ldrStatus = analogRead(ldrPin);
-if (ldrStatus <= 200) {
+int ldrStatus = analogRead(ldrPin); //Read the ADC LDR value
+if (ldrStatus <= 200) { //if adc value <= 200
 
-  digitalWrite(ledPin, HIGH);  
-  Serial.print("Its DARK, Turn on the LED : ");  
-  Serial.println(ldrStatus);
+  digitalWrite(ledPin, HIGH);  //The LED is on 
+  Serial.print("Its DARK, Turn on the LED : "); //Print string on the terminal 
+  Serial.println(ldrStatus); //Print value on the terminal
   
 } else {
 
-  digitalWrite(ledPin, LOW);  
-  Serial.print("Its BRIGHT, Turn off the LED : ");  
-  Serial.println(ldrStatus);
+  digitalWrite(ledPin, LOW);  //The LED is off
+  Serial.print("Its BRIGHT, Turn off the LED : ");  //Print string on the terminal 
+  Serial.println(ldrStatus); //Print value on the terminal
   
 }
-
+delay(1000); //delay for 1 second
 }
